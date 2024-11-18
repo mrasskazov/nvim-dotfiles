f8356bcff2c039e075f9e97db1be95492ea7ee2d
Maxim Rasskazov <m.rasskazov@gmail.com>
2024-11-19 01:29:31 +0400

    One more try of telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,6 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
+  "nvim-telescope/telescope.nvim",
+  keys = {
       -- add a keymap to browse plugin files
       -- stylua: ignore
       {
@@ -9,39 +8,44 @@ return {
         function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
         desc = "Find Plugin File",
       },
-    },
-    -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "flex",
-        layout_config = {
-          prompt_position = "bottom",
-        },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
-    },
-
-    config = function()
-      local telescope = require("telescope")
-      telescope.setup({
-        pickers = {
-          live_grep = {
-            file_ignore_patterns = { "node_modules", ".git", ".venv" },
-            additional_args = function(_)
-              return { "--hidden" }
-            end,
-          },
-          find_files = {
-            file_ignore_patterns = { "node_modules", ".git", ".venv" },
-            hidden = true,
-          },
-        },
-        extensions = {
-          "fzf",
-        },
-      })
-      telescope.load_extension("fzf")
-    end,
   },
+  -- change some options
+  opts = {
+    --  layout_strategy = "flex",
+    --defaults = {
+    --  layout_config = {
+    --    prompt_position = "bottom",
+    --  },
+    --  sorting_strategy = "ascending",
+    --  winblend = 0,
+    --},
+    layout_strategy = "flex",
+    layout_config = {
+      prompt_position = "bottom",
+    },
+    sorting_strategy = "ascending",
+    winblend = 0,
+  },
+
+  config = function()
+    local telescope = require("telescope")
+    telescope.setup({
+      pickers = {
+        live_grep = {
+          file_ignore_patterns = { "node_modules", ".git", ".venv" },
+          additional_args = function(_)
+            return { "--hidden" }
+          end,
+        },
+        find_files = {
+          file_ignore_patterns = { "node_modules", ".git", ".venv" },
+          hidden = true,
+        },
+      },
+      extensions = {
+        "fzf",
+      },
+    })
+    telescope.load_extension("fzf")
+  end,
 }
